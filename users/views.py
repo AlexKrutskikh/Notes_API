f5d7967e6cae1bfc9d544361be564e3cfce3f210
Pavel Fazluktinov <kramarenkop96@gmail.com>
2024-11-11 23:23:28 +0300

    no user id error

--- users/views.py
+++ users/views.py
@@ -33,6 +33,11 @@ class UpdateProfileFieldsView(APIView):
     def post(self, request, *args, **kwargs):
         # Получаем user_id из запроса
         user_id = request.data.get('user_id')
+        if user_id is None:
+            return Response(
+                {"detail": "Не заполнен параметр user_id."},
+                status=status.HTTP_400_BAD_REQUEST)
+
         # Ищем профиль с указанным user_id
         profile = get_object_or_404(Profile, user_id=user_id)
 
