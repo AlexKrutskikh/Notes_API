de20a1f1127e3f15c851125e407b9c0111714d23
AlexKrutskikh <krutskih.ay@gmail.com>
2024-11-22 17:16:54 +0300

    changes_in_animal

--- animals/models.py
+++ animals/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Аnimals(models.Model):
+
+    pet_art = models.CharField(max_length=100)
+    pet_weight = models.CharField(max_length=100)
+    pet_gender = models.CharField(max_length=50)
+    is_homeless = models.BooleanField()
