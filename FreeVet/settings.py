558fc50324fb9de5bd4b73754791c03f229c2f98
Андрей Метляев <andrejmetlaev@MacBook-Pro-Andrej.local>
2024-12-04 15:31:40 +0700

    add pipeline for facebook login

--- FreeVet/settings.py
+++ FreeVet/settings.py
@@ -146,11 +146,15 @@ TEMPLATES = [
 WSGI_APPLICATION = 'FreeVet.wsgi.application'
 
 DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        },
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),
     }
+}
 
 # Настройки Twilio (или Plivo)
 TWILIO_ACCOUNT_SID = env('TWILIO_ACCOUNT_SID')
