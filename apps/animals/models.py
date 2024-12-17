fac3b4acf01cd51dfd47a68aac922860c3c27e19
Alex Krutskikh <krutskih.ay@gmail.com>
2024-12-17 07:09:25 +0300

    delete_files

--- apps/animals/models.py
+++ apps/animals/models.py
@@ -18,7 +18,6 @@ class Animal(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(null=True, blank=True)
     user = models.ForeignKey(User, related_name='animal_user', on_delete=models.CASCADE)
-    photos = models.JSONField(default=list)
 
     def __str__(self):
         return self.name
