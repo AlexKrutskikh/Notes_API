d9d16d346f518e2e7d4b7f620bc356139f6fec25
Alex Krutskikh <krutskih.ay@gmail.com>
2025-01-09 09:56:09 +0300

    add_login

--- apps/profiles/signals.py
+++ apps/profiles/signals.py
@@ -1,6 +1,10 @@
+import logging
+
 from django.core.management import call_command
 from django.db.models.signals import post_migrate
 from django.dispatch import receiver
+
+logger = logging.getLogger(__name__)  # Настраиваем логгер
 
 
 @receiver(post_migrate)
@@ -9,5 +13,9 @@ def load_fixtures(sender, **kwargs):
     Автоматически загружает фикстуры после выполнения миграций.
     """
     if sender.name == "apps.profiles":
-        print("Загружаются фикстуры для приложения profiles...")
-        call_command("loaddata", "perks_fixture.json")
+        logger.info("Загружаются фикстуры для приложения profiles...")
+        try:
+            call_command("loaddata", "perks_fixture.json")
+            logger.info("Фикстуры успешно загружены.")
+        except Exception as e:
+            logger.error(f"Ошибка при загрузке фикстур: {e}")
