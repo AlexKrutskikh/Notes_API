14bfd5864aacb5254e575ddfab32e5de0a739f22
Anastasiia Kozlova <anautomnee@gmail.com>
2025-01-27 13:57:53 +0100

    Update urls

--- apps/vetbooks/urls.py
+++ apps/vetbooks/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import AddIdentification, CreateVetbook
+from .views import AddIdentification, CreateVetbook, VetbookInfo
 
 urlpatterns = [
     path("v1/vetbook/create/", CreateVetbook.as_view(), name="create_vetbook"),
+    path("/v1/vetbooks/get_info/", VetbookInfo.as_view(), name="get_vetbook"),
     path("v1/vetbook/add_identification/", AddIdentification.as_view(), name="add_identification"),
 ]
