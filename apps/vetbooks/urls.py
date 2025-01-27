bf2bf8166716e6933c04c9bcc107127d0249108f
Anastasiia Kozlova <anautomnee@gmail.com>
2025-01-27 10:05:08 +0100

    Add add_identification view and route

--- apps/vetbooks/urls.py
+++ apps/vetbooks/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import CreateVetbook
+from .views import CreateVetbook, AddIdentification
 
 urlpatterns = [
     path("v1/vetbook/create/", CreateVetbook.as_view(), name="create_vetbook"),
+    path("v1/vetbook/add_identification/", AddIdentification.as_view(), name="add_identification")
 ]
--- apps/vetbooks/views.py
+++ apps/vetbooks/views.py
@@ -5,6 +5,9 @@ from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework_simplejwt.authentication import JWTTokenUserAuthentication
 
+from .validators import validate_identification_data
+from .models import Vetbook, Identification
+
 """Сохранение в БД данных о веткнижке"""
 
 class CreateVetbook(APIView):
@@ -17,3 +20,41 @@ class CreateVetbook(APIView):
 
 
         return Response({"message": "Vetbook created successfully"}, status=status.HTTP_201_CREATED)
+    
+
+"""Сохранение в БД данных о идентификации в веткнижке и возврат ее id"""
+
+class AddIdentification(APIView):
+
+    authentication_classes = [JWTTokenUserAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        # CAN WE GET VETBOOK ID LIKE request.data.vetbook_id or request.data.get("vetbook_id", "")
+        data = request.data
+        try:
+            validated_data = validate_identification_data(data)
+        except ValidationError as e:
+            return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
+        
+        vetbook = Vetbook.objects.get(id=request.data.get("vetbook_id", ""))
+    
+        if not vetbook:
+            return Response(
+                {
+                    "error": "VetbookNotFound",
+                },
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+        
+        try:
+            identification = Identification.objects.create(
+                vetbook=vetbook,
+                chip_number=validated_data.get("chip_number"),
+                clinic_name=validated_data.get("clinic_name"),
+                chip_installation_location=validated_data.get("chip_installation_location"),
+                chip_installation_date=validated_data.get("chip_installation_date")
+            )
+            return Response({"message": "Identification for a vetbook created successfully", "identification_id": identification.id}, status=status.HTTP_201_CREATED)
+        except Exception as e:
+            return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
