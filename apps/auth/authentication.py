0b224f1e3500e42bf41c0d9e57d8fde19b4bd546
AlexKrutskikh <krutskih.ay@gmail.com>
2025-01-31 21:30:22 +0300

    add_COOKIES

---
+++ apps/auth/authentication.py
@@ -0,0 +1,22 @@
+from django.contrib.auth import get_user_model
+from rest_framework.authentication import BaseAuthentication
+from rest_framework.exceptions import AuthenticationFailed
+from rest_framework_simplejwt.tokens import AccessToken
+
+User = get_user_model()
+
+
+class CookieJWTAuthentication(BaseAuthentication):
+    def authenticate(self, request):
+        token = request.COOKIES.get("access_token")
+
+        if not token:
+            return None
+
+        try:
+            access_token = AccessToken(token)
+            user = User.objects.get(id=access_token["user_id"])
+        except Exception:
+            raise AuthenticationFailed("InvalidToken")
+
+        return (user, None)
