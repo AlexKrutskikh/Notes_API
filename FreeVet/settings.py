6e1007bcf503ceee8dd9077bb26bc44ab74a7d12
Alex Krutskikh <krutskih.ay@gmail.com>
2025-03-10 15:52:06 +0300

    add_nginx

--- FreeVet/settings.py
+++ FreeVet/settings.py
@@ -159,9 +159,6 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
-    'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.AllowAny',
-    ),
 }
 
 MIDDLEWARE = [
