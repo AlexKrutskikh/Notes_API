2dbffa383b875bf0940794078775e2fae99f3052
Alex Krutskikh <krutskih.ay@gmail.com>
2025-03-13 12:58:56 +0300

    edit_swagger_specialist

--- apps/specialist_info/views.py
+++ apps/specialist_info/views.py
@@ -4,6 +4,7 @@ from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from drf_yasg.utils import swagger_auto_schema
 
 from apps.auth.authentication import CookieJWTAuthentication
 from apps.auth.models import User
