8342048090e30190d70913909ca7bc5e12059801
Alex Krutskikh <krutskih.ay@gmail.com>
2025-03-20 14:11:55 +0300

    add_read_me

--- apps/auth/social_view.py
+++
@@ -1,9 +0,0 @@
-from django.conf import settings
-from django.http import HttpResponseRedirect
-
-"""API для авторизации и регистрации через социальные сети"""
-
-
-def google_oauth_redirect(request):
-    redirect_url = f"{settings.BASE_URL}/api/auth/social-auth/login/google-oauth2/"
-    return HttpResponseRedirect(redirect_url)
--- apps/auth/urls.py
+++ apps/auth/urls.py
@@ -2,18 +2,13 @@ from django.urls import include, path
 
 from .auth_view import AuthorizationUser, LogoutView, RegistrationUser
 from .authentication import CustomTokenRefreshView
-from .social_view import google_oauth_redirect
 
 urlpatterns = [
     path("token/refresh/", CustomTokenRefreshView.as_view(), name="token_refresh"),
-    # api бибsqlite_sequenceлиотеки social-auth-app-django
-    path("social-auth/", include("social_django.urls", namespace="social")),
-    # регистрация и авторизация google
-    path("v1/authentication/google/", google_oauth_redirect, name="google-login-shortcut"),
     # регистрация
     path("v1/registration/user/", RegistrationUser.as_view(), name="RegistrationUser"),
     # вход
     path("v1/authorization/user/", AuthorizationUser.as_view(), name="AuthorizationUser"),
     # выход
-    path("logout/", LogoutView.as_view(), name="logout"),
+    path("v1/logout/user", LogoutView.as_view(), name="logout"),
 ]
